41914d16cea21bcb8df2405d33b91d4a84cacf0a
PKroem <kroemer@outlook.com>
2020-05-20 13:56:39 +0200

    works with images now

--- example/opencv_demo.cc
+++ example/opencv_demo.cc
@@ -63,12 +63,6 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    // Initialize camera
-    VideoCapture cap(0);
-    if (!cap.isOpened()) {
-        cerr << "Couldn't open video capture device" << endl;
-        return -1;
-    }
 
     // Initialize tag detector with options
     apriltag_family_t *tf = NULL;
@@ -99,53 +93,64 @@ int main(int argc, char *argv[])
 
     Mat frame, gray;
     while (true) {
-        cap >> frame;
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        String path("ApriltagDessert/*.png"); //select only jpg
+        vector<String> fn;
+        vector<Mat> data;
+        glob(path,fn,true); // recurse
+        for (size_t k=0; k<fn.size(); ++k)
+        {
+            Mat frame = imread(fn[k]);
+            if (frame.empty()) continue; //only proceed if sucsessful
+            // you probably want to do some preprocessing
+            data.push_back(frame);        
 
-        // Make an image_u8_t header for the Mat data
-        image_u8_t im = { .width = gray.cols,
-            .height = gray.rows,
-            .stride = gray.cols,
-            .buf = gray.data
-        };
+            cvtColor(frame, gray, COLOR_BGR2GRAY);
 
-        zarray_t *detections = apriltag_detector_detect(td, &im);
-        cout << zarray_size(detections) << " tags detected" << endl;
+            // Make an image_u8_t header for the Mat data
+            image_u8_t im = { .width = gray.cols,
+                .height = gray.rows,
+                .stride = gray.cols,
+                .buf = gray.data
+            };
 
-        // Draw detection outlines
-        for (int i = 0; i < zarray_size(detections); i++) {
-            apriltag_detection_t *det;
-            zarray_get(detections, i, &det);
-            line(frame, Point(det->p[0][0], det->p[0][1]),
-                     Point(det->p[1][0], det->p[1][1]),
-                     Scalar(0, 0xff, 0), 2);
-            line(frame, Point(det->p[0][0], det->p[0][1]),
-                     Point(det->p[3][0], det->p[3][1]),
-                     Scalar(0, 0, 0xff), 2);
-            line(frame, Point(det->p[1][0], det->p[1][1]),
-                     Point(det->p[2][0], det->p[2][1]),
-                     Scalar(0xff, 0, 0), 2);
-            line(frame, Point(det->p[2][0], det->p[2][1]),
-                     Point(det->p[3][0], det->p[3][1]),
-                     Scalar(0xff, 0, 0), 2);
+            zarray_t *detections = apriltag_detector_detect(td, &im);
+            cout << zarray_size(detections) << " tags detected" << endl;
 
-            stringstream ss;
-            ss << det->id;
-            String text = ss.str();
-            int fontface = FONT_HERSHEY_SCRIPT_SIMPLEX;
-            double fontscale = 1.0;
-            int baseline;
-            Size textsize = getTextSize(text, fontface, fontscale, 2,
-                                            &baseline);
-            putText(frame, text, Point(det->c[0]-textsize.width/2,
-                                       det->c[1]+textsize.height/2),
-                    fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
+            // Draw detection outlines
+            for (int i = 0; i < zarray_size(detections); i++) {
+                apriltag_detection_t *det;
+                zarray_get(detections, i, &det);
+                line(frame, Point(det->p[0][0], det->p[0][1]),
+                        Point(det->p[1][0], det->p[1][1]),
+                        Scalar(0, 0xff, 0), 2);
+                line(frame, Point(det->p[0][0], det->p[0][1]),
+                        Point(det->p[3][0], det->p[3][1]),
+                        Scalar(0, 0, 0xff), 2);
+                line(frame, Point(det->p[1][0], det->p[1][1]),
+                        Point(det->p[2][0], det->p[2][1]),
+                        Scalar(0xff, 0, 0), 2);
+                line(frame, Point(det->p[2][0], det->p[2][1]),
+                        Point(det->p[3][0], det->p[3][1]),
+                        Scalar(0xff, 0, 0), 2);
+
+                stringstream ss;
+                ss << det->id;
+                String text = ss.str();
+                int fontface = FONT_HERSHEY_SCRIPT_SIMPLEX;
+                double fontscale = 1.0;
+                int baseline;
+                Size textsize = getTextSize(text, fontface, fontscale, 2,
+                                                &baseline);
+                putText(frame, text, Point(det->c[0]-textsize.width/2,
+                                        det->c[1]+textsize.height/2),
+                        fontface, fontscale, Scalar(0xff, 0x99, 0), 2);
+            }
+            apriltag_detections_destroy(detections);
+
+            imshow("Tag Detections", frame);
+            if (waitKey(30) >= 0)
+                break;
         }
-        apriltag_detections_destroy(detections);
-
-        imshow("Tag Detections", frame);
-        if (waitKey(30) >= 0)
-            break;
     }
 
     apriltag_detector_destroy(td);
